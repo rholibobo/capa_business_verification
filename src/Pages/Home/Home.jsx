1bdcb312450339d7ee08deb492f3e7a19f10b570
Nathaniyell <essien.nathan@yahoo.com>
2023-11-29 15:19:05 +0100

    update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <section className='flex flex-col justify-between items-center bg-blue-100 mb-10 mt-4 w-11/12 mx-auto'>
       <div className='text-center p-2'>
       <h1 className='text-4xl font-bold my-2'><span className='text-green-700'>Verify</span> any business </h1>
-      <p className='text-lg'><span className='text-orange-600 font-bold'>Capa Business Verification</span> is your all in one business verification platform. <br /> Find and connect with authentic Businesses and their owners</p>
+      <p className='text-lg'><span className='text-orange-600 font-bold'>Capa Business Verification</span> serves as your comprehensive business verification platform, offering a streamlined experience for discovering and connecting with genuine businesses and their owners. Our platform is designed to provide you with all the tools you need for reliable and authentic business verification, ensuring a secure and trustworthy environment for your business interactions</p>
       </div>
       <img src="" alt="" />
     </section>
