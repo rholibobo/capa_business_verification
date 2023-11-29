498dbf1bafc6869989d0c103f4b879da11b46ef5
Nathaniyell <essien.nathan@yahoo.com>
2023-11-29 15:02:23 +0100

    update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-4'>
-    <div className=''>
-      <h1>Verify any Business </h1>
-      <p>Capa Business Verification is your all in one business verification platform. Find and connect with authentic Businesses and their owners</p>
-    </div>
-    <section>
+    <div className='p-1'>
+    <section className='flex flex-col justify-between items-center bg-blue-100 mb-10 mt-4 w-11/12 mx-auto'>
+      <div className='text-center p-2'>
+      <h1 className='text-4xl font-bold my-2'><span className='text-green-700'>Verify</span> any business </h1>
+      <p className='text-lg'><span className='text-orange-600 font-bold'>Capa Business Verification</span> is your all in one business verification platform. <br /> Find and connect with authentic Businesses and their owners</p>
+      </div>
+      <img src="" alt="" />
+    </section>
+    <section className='w-11/12 mx-auto'>
       <h1>Verify with Business name</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto voluptatibus deleniti praesentium eaque iste obcaecati ullam placeat facilis voluptates expedita.</p>
     </section>
