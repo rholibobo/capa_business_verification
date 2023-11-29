cc1633f54adc1c34850e88ec3100e4b4aab94c2e
Nathaniyell <essien.nathan@yahoo.com>
2023-11-29 15:35:01 +0100

    update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import { Email } from '@mui/icons-material'
 
 const Home = () => {
   return (
-    <div className='p-1'>
-    <section className='flex flex-col justify-between items-center bg-blue-100 mb-10 mt-4 w-11/12 mx-auto'>
+    <div className=''>
+    <section className='flex flex-col justify-between items-center bg-green-100 mb-10'>
       <div className='text-center p-2'>
       <h1 className='text-4xl font-bold my-2'><span className='text-green-700'>Verify</span> any business </h1>
       <p className='text-lg'><span className='text-orange-600 font-bold'>Capa Business Verification</span> serves as your comprehensive business verification platform, offering a streamlined experience for discovering and connecting with genuine businesses and their owners. Our platform is designed to provide you with all the tools you need for reliable and authentic business verification, ensuring a secure and trustworthy environment for your business interactions</p>
@@ -13,7 +13,9 @@ const Home = () => {
     </section>
     <section className='w-11/12 mx-auto text-center p-2'>
       <h1 className='text-2xl font-bold my-2'>Verify with Business name</h1>
-      <p>Experience hassle-free verification by simply typing in the business name. Our 'Verify with Business Name' feature puts the power in your hands, allowing you to effortlessly authenticate businesses by entering their names. Swift, accurate, and user-friendly – confirming business details has never been this easy.</p>
+      <p>Experience hassle-free verification by simply typing in the business name. Our <span className='italic text-green-600'>
+      Verify with Business Name
+        </span>  feature puts the power in your hands, allowing you to effortlessly authenticate businesses by entering their names. Swift, accurate, and user-friendly – confirming business details has never been this easy.</p>
     </section>
     <section className='w-11/12 mx-auto p-2 flex justify-between items-center md:flex-row-reverse flex-col'>
      <div className='w-3/5'>
