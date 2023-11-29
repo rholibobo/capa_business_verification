536c32ecd43bf9b97631854e5475d7eb417e9f9a
Nathaniyell <essien.nathan@yahoo.com>
2023-11-29 15:39:28 +0100

    update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,9 +4,9 @@ import { Email } from '@mui/icons-material'
 const Home = () => {
   return (
     <div className=''>
-    <section className='flex flex-col justify-between items-center bg-green-100 mb-10'>
-      <div className='text-center p-2'>
-      <h1 className='text-4xl font-bold my-2'><span className='text-green-700'>Verify</span> any business </h1>
+    <section className='flex flex-col justify-between items-center bg-green-100 mb-10 text-center px-4 py-8 text-green-800'>
+      <div className=''>
+      <h1 className='text-4xl font-bold my-2'><span className='text-black'>Verify</span> any business </h1>
       <p className='text-lg'><span className='text-orange-600 font-bold'>Capa Business Verification</span> serves as your comprehensive business verification platform, offering a streamlined experience for discovering and connecting with genuine businesses and their owners. Our platform is designed to provide you with all the tools you need for reliable and authentic business verification, ensuring a secure and trustworthy environment for your business interactions</p>
       </div>
       <img src="" alt="" />
@@ -17,8 +17,8 @@ const Home = () => {
       Verify with Business Name
         </span>  feature puts the power in your hands, allowing you to effortlessly authenticate businesses by entering their names. Swift, accurate, and user-friendly – confirming business details has never been this easy.</p>
     </section>
-    <section className='w-11/12 mx-auto p-2 flex justify-between items-center md:flex-row-reverse flex-col'>
-     <div className='w-3/5'>
+    <section className='w-11/12 mx-auto p-2 flex justify-between items-center md:flex-row-reverse flex-col-reverse'>
+     <div className='md:w-3/5'>
      <h1 className='text-2xl font-bold my-2'>Verify with Business Email</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto voluptatibus deleniti praesentium eaque iste obcaecati ullam placeat facilis voluptates expedita.</p>
      </div>
