85382e415fcecb02a05b0ed4f812cb516cdf4487
Nathaniyell <essien.nathan@yahoo.com>
2023-11-29 14:45:54 +0100

    update Navbar.jsx, Home.jsx and src/index.css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className='p-4'>
+      <h1>Verify any Business </h1>
+      <p>Capa Business Verification is your all in one business verification platform. Find and connect with authentic Businesses and their owners</p>
+    </div>
+    </>
   )
 }
 
