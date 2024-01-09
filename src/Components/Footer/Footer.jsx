13da03f5a8f6bccf421dda990cdd58a490ef61b1
Nathaniyell <essien.nathan@yahoo.com>
2024-01-09 20:15:35 +0100

    update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,18 +7,22 @@ const Footer = () => {
       <div className="md:w-10/12 grid grid-cols-1 md:grid-cols-3 items-center gap-4 border-white border-b mx-auto py-10">
         <h5 className="text-3xl font-bold tracking-widest">LOGO</h5>
         <div>
-          <h5 className="text-2xl font-semibold tracking-widest">Solutions</h5>
-          <ul>
+          <h5 className="text-2xl font-semibold tracking-widest mb-6">
+            Solutions
+          </h5>
+          <Stack direction="column" spacing="2">
             <li>Business Verification</li>
             <li>Email Verification</li>
-          </ul>
+          </Stack>
         </div>
         <div>
-          <h5 className="text-2xl font-semibold tracking-widest">Use Cases</h5>
-          <ul>
+          <h5 className="text-2xl font-semibold tracking-widest mb-6">
+            Use Cases
+          </h5>
+          <Stack direction="column" spacing="3">
             <li>Lorem1</li>
             <li>Lorem2</li>
-          </ul>
+          </Stack>
         </div>
       </div>
       <div className="mt-8 md:w-11/12 mx-auto grid grid-cols-1 md:grid-cols-3 items-center gap-2">
