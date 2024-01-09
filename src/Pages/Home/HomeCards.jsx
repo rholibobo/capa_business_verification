e77da6aa9bc7b4febf9229e213014c3d65e2081b
Nathaniyell <essien.nathan@yahoo.com>
2024-01-09 04:26:09 +0100

    update HomeCards.jsx

--- src/Pages/Home/HomeCards.jsx
+++ src/Pages/Home/HomeCards.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const HomeCards = () => {
+export const HomeCards = ({title, description, linkDestination}) => {
   return (
-    <div>HomeCards</div>
+    <div className='p-12 lg:p-16 border border-gray-900 rounded-5xl'>
+    <h2 className='mb-6 text-5xl text-white tracking-5xl'>{title}</h2>a
+    <p className='mb-24 lg:mb-56 text-lg text-white text-opacity-60'>{description}</p>
+    <Link className='mr-3.5 text-white font-medium underline group inline-flex items-center'>Learn more</Link>
+        </div>
   )
 }
