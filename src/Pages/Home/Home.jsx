1b37b20ab32388d0da7ff7ecf79af63b9898ac61
Nathaniyell <essien.nathan@yahoo.com>
2024-01-09 13:42:13 +0100

    update Home.jsx and HomeCards.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
     title: "Verify with Business name",
     description: "Experience hassle-free verification by simply typing in the business name. Our Verify with Business Namefeature puts the power in your hands, allowing you to effortlessly authenticate businesses by entering their names. Swift, accurate, and user-friendly",
     link: "/",
-    avatar: "" ,
+    avatar: "../../assets/undraw_authentication_re_svpt.svg" ,
   },
   {
     title: "Verify with business email",
     description: "Seamlessly confirm identities and ensure trust by leveraging our 'verify with email' feature - With just an email address, you can authenticate users and businesses, adding an extra layer of security to your interactions",
     link: "/",
-    avatar: "" ,
+    avatar: "../../assets/undraw_personal_email_re_4lx7.svg" ,
   },
 ]
   return (
@@ -26,8 +26,9 @@ const Home = () => {
       </div>
       <img src={heroImage} className='md:w-2/5' alt="Hero" />
     </section>
+    <div>
+      <h2 className='text-center text-2xl leading-[5]'>Reliable end-to-end verification services</h2>
     <section className='w-11/12 md:w-10/12 mx-auto p-2 flex flex-col md:flex-row gap-6 items-center justify-between'>
-      <h2 className='text-center text-2xl'>Reliable end-to-end verification services</h2>
       {cardDetails.map((card, index)=>{
         return(
 
@@ -36,6 +37,7 @@ const Home = () => {
       })}
       
     </section>
+    </div>
   
     </div>
   )
--- src/Pages/Home/HomeCards.jsx
+++ src/Pages/Home/HomeCards.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-export const HomeCards = ({ title, description, linkDestination }) => {
+export const HomeCards = ({ title, description, linkDestination, avatar }) => {
   return (
-    <div className="p-6 lg:p-8 border border-gray-900 rounded-md">
+    <div className="p-6 lg:p-8 border border-green-900 rounded-md">
+        <img src={avatar} alt="card-img" />
       <h2 className="mb-6 text-3xl  tracking-5xl">{title}</h2>
       <p className="mb-16 text-lg text-opacity-60">{description}</p>
       <Link
